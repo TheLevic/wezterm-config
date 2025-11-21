9e197d9896d1256f6d40e642935d1f513c26823f
Levi <levicrider08@gmail.com>
2025-11-20 20:10:25 -0600

    fix

--- wezterm.lua
+++ wezterm.lua
@@ -1,1 +1,16 @@
-/Users/thelevic/.wezterm.lua
+local wezterm = require("wezterm")
+
+return {
+    color_scheme = "Tokyo Night",  -- Or any scheme you like
+    enable_tab_bar = false,
+
+    -- TRUECOLOR SUPPORT
+    term = "xterm-256color",
+
+    -- Required to make Neovim + tmux show correct colors
+    colors = {
+        foreground = "#c0caf5",
+        background = "#1a1b26",
+    },
+    font_size=15.0,
+}
